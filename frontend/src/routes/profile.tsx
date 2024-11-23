7376e5cffecafb2e352d03d4e71edb16c0629d9a
NopalAul <mnaufal0605@gmail.com>
2024-11-23 19:48:47 +0700

    feat: add profile page fe (dummy)

--- frontend/src/routes/profile.tsx
+++ frontend/src/routes/profile.tsx
@@ -1,6 +1,45 @@
+import React from 'react';
+import ProfileCard from '../components/profile/ProfileCard';
+import ProfileDetails from '../components/profile/ProfileDetails';
 
-export default function Profile() {
-    return <>
-        <h1>Profile</h1>
-    </>
+type AccessLevel = "public" | "owner" | "connected" | "notConnected";
+
+interface ProfileProps {
+    // accessLevel: AccessLevel;
+    accessLevel?: AccessLevel;
 }
+
+const Profile: React.FC<ProfileProps> = ({ accessLevel = "notConnected" }) => {
+  const isOwner = accessLevel === "owner";
+  const isConnected = accessLevel === "connected";
+
+  const profileData = {
+    name: "Ucok Baba Manihot",
+    banner: "",
+    photo: "",
+    description: "Informatics Student at Bandung Institute of Technology",
+    connections: 20,
+  };
+
+  return (
+    <div className="min-h-screen">
+      <div className="">
+        <ProfileCard
+          banner={profileData.banner}
+          photo={profileData.photo}
+          name={profileData.name}
+          description={profileData.description}
+          connections={profileData.connections}
+          accessLevel={accessLevel}
+          isOwner={isOwner}     
+          isConnected={isConnected}
+        />
+        <ProfileDetails
+          accessLevel={accessLevel}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;

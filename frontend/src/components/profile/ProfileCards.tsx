42b8ff36441569d514897b577d072e0f9bb8bf25
NopalAul <mnaufal0605@gmail.com>
2024-11-25 21:44:51 +0700

    feat: delete description & adjust info on acces level profile

--- frontend/src/components/profile/ProfileCards.tsx
+++ frontend/src/components/profile/ProfileCards.tsx
@@ -14,33 +14,12 @@ const ProfileCards: React.FC<ProfileCardsProps> = ({
 
   return (
     <div className="mb-6 mx-80">
-      {!isPublic && (
-        <>
-          {/* Postingan relevan?*/}
-          {(isOwner || isConnected) && (
-            <div className="bg-white border border-gray-300 rounded-lg relative mt-4 p-6">
-              <div className="flex justify-between items-center">
-                <h3 className="text-xl font-semibold">Post</h3>
-                {isOwner && (
-                  <button className="p-2 bg-white hover:bg-gray-200 rounded-full">
-                    <img
-                      src={EditIcon}
-                      alt="Edit Icon"
-                      className="w-[24px] h-[24px]"
-                    />
-                  </button>
-                )}
-              </div>
-              <p className="text-gray-600 mt-2">
-                Postingan relevan goes here.... (dinamis kah?)
-              </p>
-            </div>
-          )}
-
-          {/* Experience Card */}
+      <>
+        {/* Postingan relevan? */}
+        {(isOwner || isConnected) && !isPublic && (
           <div className="bg-white border border-gray-300 rounded-lg relative mt-4 p-6">
             <div className="flex justify-between items-center">
-              <h3 className="text-xl font-semibold">Experience</h3>
+              <h3 className="text-xl font-semibold">Post</h3>
               {isOwner && (
                 <button className="p-2 bg-white hover:bg-gray-200 rounded-full">
                   <img
@@ -52,32 +31,49 @@ const ProfileCards: React.FC<ProfileCardsProps> = ({
               )}
             </div>
             <p className="text-gray-600 mt-2">
-              Isinya list card pengalaman kerja 
+              Postingan relevan goes here.... (dinamis kah?)
             </p>
           </div>
+        )}
 
-          {/* Skills Card (for isConnected or owner) */}
-          {(isOwner || isConnected) && (
-            <div className="bg-white border border-gray-300 rounded-lg relative mt-4 p-6">
-              <div className="flex justify-between items-center">
-                <h3 className="text-xl font-semibold">Skills</h3>
-                {isOwner && (
-                  <button className="p-2 bg-white hover:bg-gray-200 rounded-full">
-                    <img
-                      src={EditIcon}
-                      alt="Edit Icon"
-                      className="w-[24px] h-[24px]"
-                    />
-                  </button>
-                )}
-              </div>
-              <p className="text-gray-600 mt-2">
-                Isinya list card keterampilan
-              </p>
-            </div>
-          )}
-        </>
-      )}
+        {/* Experience Card (Riwayat Pekerjaan) */}
+        <div className="bg-white border border-gray-300 rounded-lg relative mt-4 p-6">
+          <div className="flex justify-between items-center">
+            <h3 className="text-xl font-semibold">Experience</h3>
+            {isOwner && (
+              <button className="p-2 bg-white hover:bg-gray-200 rounded-full">
+                <img
+                  src={EditIcon}
+                  alt="Edit Icon"
+                  className="w-[24px] h-[24px]"
+                />
+              </button>
+            )}
+          </div>
+          <p className="text-gray-600 mt-2">
+            Isinya list card riwayat kerja 
+          </p>
+        </div>
+
+        {/* Skills Card (for isConnected or owner) */}
+        <div className="bg-white border border-gray-300 rounded-lg relative mt-4 p-6">
+          <div className="flex justify-between items-center">
+            <h3 className="text-xl font-semibold">Skills</h3>
+            {isOwner && (
+              <button className="p-2 bg-white hover:bg-gray-200 rounded-full">
+                <img
+                  src={EditIcon}
+                  alt="Edit Icon"
+                  className="w-[24px] h-[24px]"
+                />
+              </button>
+            )}
+          </div>
+          <p className="text-gray-600 mt-2">
+            Isinya list card keterampilan
+          </p>
+        </div>
+      </>
     </div>
   );
 };

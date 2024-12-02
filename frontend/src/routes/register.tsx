4b479aba5f0580f31c0b30884c58de64167cde8e
NopalAul <mnaufal0605@gmail.com>
2024-12-02 19:11:29 +0700

    feat: register page styling

--- frontend/src/routes/register.tsx
+++ frontend/src/routes/register.tsx
@@ -50,9 +50,9 @@ export default function Register({ onRegister }: { onRegister?: () => void }) {
 
   return (
 <>
-<section className="sm:bg-slate-200 bg-slate-100"> {/* Fallback */}
+<section className="sm:bg-background_grey bg-white"> {/* Fallback */}
   <div className="flex min-h-dvh min-h-screen items-center justify-center px-5 mx-auto">
-    <div className="w-full rounded-2xl md:mt-0 sm:max-w-md bg-slate-100 border-none p-0 sm:p-8">
+    <div className="w-full rounded-2xl md:mt-0 sm:max-w-md bg-white sm:border border-gray-300 p-0 sm:p-8">
       <h1 className="text-3xl font-bold text-slate-700">Register</h1>
       <p className="text-slate-400 mb-4 mt-1">Make the most of your professional life</p>
 
@@ -63,10 +63,10 @@ export default function Register({ onRegister }: { onRegister?: () => void }) {
         <FormInput register={register} error={errors.password} field="password" type="password" title="Enter Your Password"></FormInput>
         <FormInput register={register} error={errors.confirmPassword} field={"confirmPassword"} type={"password"} title={"Confirm Your Password"}></FormInput>
 
-        <button disabled={isSubmitting} className="rounded-lg bg-blue-600 hover:bg-blue-700 focus:ring-4 ring-blue-500 py-2 font-bold text-white w-full">Register</button>
+        <button disabled={isSubmitting} className="rounded-lg bg-blue_primary hover:bg-blue_hover focus:ring-4 ring-blue-500 py-2 font-bold text-white w-full">Register</button>
       </form>
 
-      <p className="text-slate-500 text-sm mt-3 text-center">Already have an account ? <Link className="hover:underline hover:text-blue-700 focus:text-blue-800 focus:underline" to={"/login"}>Login</Link></p>
+      <p className="text-slate-500 text-sm mt-3 text-center">Already have an account ? <Link className="hover:underline hover:text-blue-700 focus:text-blue_primary focus:underline" to={"/login"}>Login</Link></p>
 
     </div>
   </div>

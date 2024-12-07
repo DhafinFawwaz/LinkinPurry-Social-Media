80e5e061dd189c56beade2d85735267241094bb2
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-12-07 13:57:18 +0700

    nit: temporarily not using redis

--- backend/src/db/redis.ts
+++ backend/src/db/redis.ts
@@ -1,12 +1,13 @@
 
 import { Redis } from 'ioredis'
 
-export const redis = new Redis({
-	host: (process.env.REDIS_HOST || 'localhost') as string,
-	port: (process.env.REDIS_PORT || 6379) as number
-});
-	redis.on('error', (err) => {
-	console.log("host: " + (process.env.REDIS_HOST || 'localhost'))
-	console.log("post: " + ((process.env.REDIS_PORT || 6379) as number))
-	console.error(err)
-});
+// export const redis = new Redis({
+// 	host: (process.env.REDIS_HOST || 'localhost') as string,
+// 	port: (process.env.REDIS_PORT || 6379) as number
+// });
+// 	redis.on('error', (err) => {
+// 	console.log("host: " + (process.env.REDIS_HOST || 'localhost'))
+// 	console.log("post: " + ((process.env.REDIS_PORT || 6379) as number))
+// 	console.error(err)
+// });
+export let redis: Redis
